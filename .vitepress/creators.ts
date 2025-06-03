4b00d4d6849e907af762ac0d9c8175e37c5df294
Ritazz-L <2440385910@qq.com>
2025-06-03 16:00:59 +0800

    1

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -23,16 +23,16 @@ function getAvatarUrl(username: string): string {
 
 export const creators: Creator[] = [
   {
-    name: 'ZZBaoa',
+    name: 'AOLeomeoww',
     avatar: 'my-avator.jpg',
-    username: 'Ritazz-L',
+    username: 'AOLeomeoww',
     title: '随机爱好匹配中',
     desc: '代码小白爱好者，日常写作，插件分享，设计建模等',
     links: [
       { type: 'github', icon: 'github', link: 'https://github.com/Ritazz-L' },
       { type: 'discord', icon: 'discord', link: 'https://discord.gg/fArZxf8p' },
     ],
-    nameAliases: ['ZZBaoa', 'Ritazz-L', '龙景茶'],
+    nameAliases: ['ZZBaoa', 'Ritazz-L', '龙景茶','AOLeomeoww'],
     emailAliases: ['kranchen@foxmail.com'],
   },
 
